d4a4031326101bea0b0b74ed717f2bf7d4c6e26a
maxlin01116 <maxlin0116@gmail.com>
2026-04-04 12:44:20 +0800

    RFID.h tidy up

--- lib/RFID.h
+++ lib/RFID.h
@@ -22,21 +22,21 @@ inline byte* rfid(byte& idSize) {
         byte* id = mfrc522.uid.uidByte;  // 取得卡片的UID
         idSize = mfrc522.uid.size;       // 取得UID的長度
 
-// Serial.print("PICC type: ");      // 顯示卡片類型
-//  根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
-// MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-// Serial.println(mfrc522.PICC_GetTypeName(piccType));
-#ifdef DEBUG
-        Serial.print("UID Size: ");  // 顯示卡片的UID長度值
-        Serial.println(idSize);
-        for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
-            Serial.print("id[");
-            Serial.print(i);
-            Serial.print("]: ");
-            Serial.println(id[i], HEX);  // 以16進位顯示UID值
-        }
-        Serial.println();
-#endif
+		// Serial.print("PICC type: ");      // 顯示卡片類型
+		//  根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
+		// MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+		// Serial.println(mfrc522.PICC_GetTypeName(piccType));
+		#ifdef DEBUG
+				Serial.print("UID Size: ");  // 顯示卡片的UID長度值
+				Serial.println(idSize);
+				for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
+					Serial.print("id[");
+					Serial.print(i);
+					Serial.print("]: ");
+					Serial.println(id[i], HEX);  // 以16進位顯示UID值
+				}
+				Serial.println();
+		#endif
         mfrc522.PICC_HaltA();  // 讓卡片進入停止模式
         return id;
     }
